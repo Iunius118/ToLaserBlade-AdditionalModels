0c100937ad1ae75afe48ad2cd3bb6a1a3cb1a108
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-01-22 22:26:59 +0900

    Add model changing recipes to generator

--- src/main/java/com/github/iunius118/tolaserblade/exmodels/data/ExModelsRecipeProvider.java
+++ src/main/java/com/github/iunius118/tolaserblade/exmodels/data/ExModelsRecipeProvider.java
@@ -7,7 +7,6 @@ import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
 import java.util.function.Consumer;
@@ -25,11 +24,19 @@ public class ExModelsRecipeProvider extends RecipeProvider implements ICondition
         addModelChangeRecipes(consumer, Ingredient.of(Items.DRAGON_BREATH), 305);
         addModelChangeRecipes(consumer, Ingredient.of(Items.SHIELD), 316);
         addModelChangeRecipes(consumer, Ingredient.of(Items.SLIME_BALL), 407);
-        addModelChangeRecipes(consumer, Ingredient.of(Blocks.DIAMOND_ORE), 504);
-        addModelChangeRecipes(consumer, Ingredient.of(Blocks.DIAMOND_ORE), 519);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.DIAMOND_ORE), 504);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.DIAMOND_ORE), 519);
         addModelChangeRecipes(consumer, Ingredient.of(Items.HONEYCOMB), 606);
         addModelChangeRecipes(consumer, Ingredient.of(Items.COD), 726);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.CROSSBOW), 801);
         addModelChangeRecipes(consumer, Ingredient.of(Items.TROPICAL_FISH), 808);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.BARREL), 825);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.LIGHTNING_ROD), 903);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.PISTON), 913);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.SPYGLASS), 924);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.IRON_AXE), 1009);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.WOODEN_SWORD), 1108);
+        addModelChangeRecipes(consumer, Ingredient.of(Items.STONE_SWORD), 1216);
         addModelChangeRecipes(consumer, Ingredient.of(Items.END_CRYSTAL), 1221);
     }
 
